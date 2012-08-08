ecdb8258d8603c9e34476b77517d86f23f5e7b68
oscar costeno <emilianocosteno@gmail.com>
2012-08-08 17:45:06 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Demoprueba::Application.config.secret_token = '3696f2f62cd0730c6d1d509f5ff1d0bc8512b4bbed15acac282bfb82d36e243ece6692c360ab26392c2136b4743ec10cd3159569f4c8f550f0c8614f2c2dc5c3'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Demoprueba::Application.config.session_store :cookie_store, key: '_demoprueba_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Demoprueba::Application.config.session_store :active_record_store
